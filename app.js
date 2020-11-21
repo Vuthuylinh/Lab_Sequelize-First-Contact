f04200993dd7bafff29422302abbcc2f31c42d12
Linh Vu <thuylinh2490@gmail.com>
2020-11-20 23:30:19 -0500

    finished setup

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const morgan = require('morgan');
-const app = express();
+// const bodyParser= require('body-parser'); new version of express has built-in body-parser
+const path = require('path')
+const app = express(); // create an express app
+const layout = require("./views/layout");
 
-app.use(morgan("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(morgan("dev")); //loging middleware
+app.use(express.static(path.join(__dirname + "./public")));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-  res.send("hello world");
+  res.send(layout("hello world!"));
 })
 
 
 
-const PORT = 1337;
-
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+module.exports = app;
 
